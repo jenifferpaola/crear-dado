854a2b76aa3cf73541b88d9d14aa5e8ada86be0a
jenifferpaola <146844807+jenifferpaola@users.noreply.github.com>
2023-10-03 08:09:15 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,57 @@
+let Número_dado = 0
+input.onGesture(Gesture.Shake, function () {
+    Número_dado = randint(1, 6)
+    music.play(music.builtinPlayableSoundEffect(soundExpression.spring), music.PlaybackMode.UntilDone)
+    if (Número_dado == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Número_dado == 2) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (Número_dado == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (Número_dado == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (Número_dado == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (Número_dado == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
